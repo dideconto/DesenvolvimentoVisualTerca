80f79a3808f3672ed1d4783de81be199eb1ff0cb
aluno <Aluno@DOSO027251.educacional.up>
2022-08-30 10:28:53 -0300

    API Folha - Alterar e remover funcionário

--- API_Folha/Controllers/FuncionarioController.cs
+++ API_Folha/Controllers/FuncionarioController.cs
@@ -14,10 +14,7 @@ namespace API.Controllers
         // GET: /api/funcionario/listar
         [HttpGet]
         [Route("listar")]
-        public IActionResult Listar()
-        {
-            return Ok(funcionarios);
-        }
+        public IActionResult Listar() => Ok(funcionarios);
 
         // POST: /api/funcionario/cadastrar
         [HttpPost]
@@ -30,18 +27,46 @@ namespace API.Controllers
 
         // GET: /api/funcionario/buscar/{cpf}
         [HttpGet]
-        [Route("buscar/{cpf}")] 
+        [Route("buscar/{cpf}")]
         public IActionResult Buscar([FromRoute] string cpf)
+        {
+            Funcionario funcionario = funcionarios.
+                FirstOrDefault(f => f.Cpf.Equals(cpf));
+            return funcionario != null ? Ok(funcionario) : NotFound();
+        }
+
+        // DELETE: /api/funcionario/deletar/{cpf}
+        [HttpDelete]
+        [Route("deletar/{cpf}")]
+        public IActionResult Deletar([FromRoute] string cpf)
         {
             Funcionario funcionario = funcionarios.FirstOrDefault
             (
                 f => f.Cpf.Equals(cpf)
             );
-            if (funcionario == null)
+            if(funcionario != null)
             {
-                return NotFound();
+                funcionarios.Remove(funcionario);
+                return Ok(funcionario);
             }
-            return Ok(funcionario);
+            return NotFound();
+        }
+
+        // PATCH: /api/funcionario/alterar
+        [HttpPatch]
+        [Route("alterar")]
+        public IActionResult Alterar([FromBody] Funcionario funcionario)
+        {
+            Funcionario funcionarioEncontrado = funcionarios.FirstOrDefault
+            (
+                f => f.Cpf.Equals(funcionario.Cpf)
+            );
+            if(funcionarioEncontrado != null)
+            {
+                funcionarioEncontrado.Nome = funcionario.Nome;
+                return Ok(funcionarioEncontrado);
+            }
+            return NotFound();
         }
     }
 }
