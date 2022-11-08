c25b8717f3872c5cecd5c6a5fbae21f61b52734d
aluno <Aluno@DOSO027251.educacional.up>
2022-11-08 12:22:18 -0300

    FRONT Folhas - Cadastrar folha

--- FRONT_Folhas/src/app/components/pages/folha/cadastrar-folha/cadastrar-folha.component.ts
+++ FRONT_Folhas/src/app/components/pages/folha/cadastrar-folha/cadastrar-folha.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Folha } from 'src/app/models/Folha';
+import { Funcionario } from 'src/app/models/Funcionario';
 
 @Component({
   selector: 'app-cadastrar-folha',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar-folha.component.css']
 })
 export class CadastrarFolhaComponent implements OnInit {
+  
+  //Template - Material Design Angular
 
-  constructor() { }
+  valor!: number;
+  quantidade!: number;
+  data!: string;
+  funcionarios!: Funcionario[];
+  funcionarioId!: number;
+
+  constructor(private http: HttpClient,
+    private router: Router) { }
 
   ngOnInit(): void {
+    //Configuração da requisição
+    this.http.get<Funcionario[]>
+      ("https://localhost:5001/api/funcionario/listar")
+      // Execução da requisição
+      .subscribe({
+        next: (funcionarios) => {
+          // console.table(funcionarios);
+          this.funcionarios = funcionarios;
+        }
+      });
+  }
+
+  cadastrar(): void{
+    let dataConvertida = new Date(this.data);
+    let folha : Folha = {
+      valorHora : this.valor,
+      quantidadeHoras : this.quantidade,
+      mes : dataConvertida.getMonth() + 1,
+      ano : dataConvertida.getFullYear(),
+      funcionarioId : this.funcionarioId
+    };
+    console.log(folha);
+    this.http
+      .post<Folha>(
+        "https://localhost:5001/api/folha/cadastrar",
+        folha
+      )
+      .subscribe({
+        next: (funcionario) => {
+          this.router.navigate(["pages/funcionario/listar"]);
+        },
+      });
   }
 
 }
---
+++ FRONT_Folhas/src/app/models/Folha.ts
@@ -0,0 +1,8 @@
+export interface Folha{
+    id?: number;
+    valorHora: number;
+    quantidadeHoras: number;
+    mes: number;
+    ano: number;
+    funcionarioId: number;
+}
